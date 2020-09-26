40e2ff45957d6f006facc7154cf9a2311a11f1c6
Carla Lara <carla@wiris.com>
2020-09-27 00:33:15 +0200

    Improve markdown script to create html files
    Create a json file that contains the paths, names and destinations.
    Improve the markdown script to read react and angular readme files and
    convert them into html files

--- scripts/services/markdownit.js
+++ scripts/services/markdownit.js
@@ -3,17 +3,71 @@
  * the conversion from md to html
  */
 
+/**
+    const fs = require('fs');
+    const MarkdownIt = require('markdown-it');
+
+    // Create the markdown variable
+    const md = new MarkdownIt();
+
+    // Read the readme file of interest
+    const markdownData = fs.readFileSync('README.md', 'utf8');
+
+    // Render the markdown data to html data
+    const result = md.render(markdownData);
+
+    // Write the html data to a file
+    fs.writeFileSync('foo.html', result);
+*/
+
 const fs = require('fs');
 const MarkdownIt = require('markdown-it');
 
 // Create the markdown variable
 const md = new MarkdownIt();
 
-// Read the readme file of interest
-const markdownData = fs.readFileSync('README.md', 'utf8');
+function readWriteFile(file, dir, name) {
+    // Create folder if it does not exist
+    fs.mkdirSync(dir, { recursive: true });
 
-// Render the markdown data to html data
-const result = md.render(markdownData);
+    // Read the readme file of interest
+    const markdownData = fs.readFileSync(file, 'utf8');
+    
+    // Render the markdown data to html data
+    const markdownRendered = md.render(markdownData);
 
-// Write the html data to a file
-fs.writeFileSync('foo.html', result);
+    // Write the html data to a file
+    fs.writeFileSync(dir + name, markdownRendered);
+}
+
+const writeFiles = () => new Promise((resolve) => {
+    // require the folder that contains the files paths 
+    const mdFolders = require('./md.json'); // eslint-disable-line global-require
+  
+    // Save all the routes in a object to run the test execution in one line
+    const mdPaths = Object.values(mdFolders);  //eslint-disable-line
+  
+    resolve(
+      Promise.all(
+        mdPaths.map(async (file) => { 
+            readWriteFile(file.path, file.dest, file.name);
+        }), 
+      ),
+    );
+  });
+
+// This file is being executed as a script.
+if (!module.parent) {
+    // Process args
+    const args = process.argv.slice(2);
+  
+    // Log a warning if there are more than 0 arguments
+    if (args.length > 0) {
+      emitWarning('No parameters needed, all the additional parameters will be ignored.');
+    }
+  
+    // Execute all the tests and resolve when finished
+    writeFiles();
+  } else { // This file is being imported as a module.
+    module.exports = markdownit;
+  }
